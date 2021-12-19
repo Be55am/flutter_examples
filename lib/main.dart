a32ed173e63e8a252877ff94c34467355d66603d
Bessam <sahlibessam@gmail.com>
2021-12-19 17:10:54 +0100

    1/ Basic layouts: Adding Images and Gradiant

--- lib/main.dart
+++ lib/main.dart
@@ -18,19 +18,27 @@ class MyApp extends StatelessWidget {
           children: [
             Container(
               margin: EdgeInsets.all(101),
-              width: 100,
-              height: 100,
+              width: 200,
+              height: 200,
+              // child: FlutterLogo(),
               decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                      'http://bit.ly/flutter_tiger'),
+                  fit: BoxFit.scaleDown,
+                ),
                 // color: Colors.red,
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(10),
+                shape: BoxShape.circle,
+                // borderRadius: BorderRadius.circular(10),
                 gradient: LinearGradient(
                   colors: [Colors.red, Colors.blue, Colors.green],
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight,
                 ),
               ),
-            )
+            ),
+            Text('Made by Bessam'),
+            Text('With Love'),
           ],
         )
       ),
