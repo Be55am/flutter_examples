679a5d840fe1f6926dfad6e2019a4083dbfa649a
Bessam <sahlibessam@gmail.com>
2021-12-20 12:35:34 +0100

    rows and collumns

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,11 +12,46 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Rows and Columns'),
         ),
-        body: Center(
-          child: Text('Hello world'),
-        ),
+        body: Home(),
       ),
     );
   }
 }
 
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final sizeX = MediaQuery.of(context).size.width;
+    final sizeY = MediaQuery.of(context).size.height;
+    return Container(
+      width: sizeX,
+      height: sizeY,
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: createSquares(3),
+      ),
+    );
+  }
+
+  List<Widget> createSquares(int numSquares) {
+    int i = 0;
+    List colors = [Colors.green, Colors.red, Colors.white];
+    List<Widget> squares = [];
+    while (i < numSquares) {
+      squares.add(Expanded(
+        flex: i,
+          child: Container(
+        width: 100,
+        height: 100,
+        color: colors[i % colors.length],
+        child: Text('Square $i'),
+      )));
+      i++;
+    }
+
+    return squares;
+  }
+}
